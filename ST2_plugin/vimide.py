fdaa568adc8ee09111dfb228b035d7a4734325eb
AMPivovarov <AMPivovarov@gmail.com>
2013-11-13 02:18:30 +0400

    fix multiwindow navigation. Another try

--- ST2_plugin/vimide.py
+++ ST2_plugin/vimide.py
@@ -38,17 +38,17 @@ def goto(view, pos):
 
 class GotoPosCommand(sublime_plugin.TextCommand):
     def run(self, edit, filename, row_b, col_b, row_e, col_e):
-        view = self.view.window().open_file(filename);
+        view = self.view.window().open_file(filename + ":" + str(row_b) + ":" + str(col_b), sublime.ENCODED_POSITION);
 
-        pt_b = view.text_point(row_b - 1, col_b)
-        pt_e = view.text_point(row_e - 1, col_e)
+        # pt_b = view.text_point(row_b - 1, col_b)
+        # pt_e = view.text_point(row_e - 1, col_e)
 
-        r = sublime.Region(pt_b, pt_e)
+        # r = sublime.Region(pt_b, pt_e)
 
-        view.sel().clear()
-        view.sel().add(r)
+        # view.sel().clear()
+        # view.sel().add(r)
 
-        view.show(r)
+        # view.show(r)
 
 class VimideDefinitionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
